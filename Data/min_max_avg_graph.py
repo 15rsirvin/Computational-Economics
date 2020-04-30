d292ad627eea2ce595b8d934ee40609da338c3a1
15rsirvin <irvinr@reed.edu>
2020-04-29 19:59:57 -0700

    final simulation/graph versions

--- Data/min_max_avg_graph.py
+++ Data/min_max_avg_graph.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 #This data is a list of lists. Each sublist at position i is some n numner of results of POA
 #for simulating with i+2 numner of bidders
-data = np.loadtxt('symmetric_data.dat')
+data = np.loadtxt('asymmetric_data.dat')
 
 # calculate min, max, avg for each # of bidders
 minimums =[min(curr) for curr in data]
@@ -12,9 +12,9 @@ maximums =[max(curr) for curr in data]
 averages = [statistics.mean(curr) for curr in data]
 x_axis = [i+2 for i in range(len(maximums))]
 
-plt.plot(x_axis, minimums, label = "Minima")
-plt.plot(x_axis, averages, label = "Averages")
-plt.plot(x_axis, maximums, label = "Maxima")
+plt.plot(x_axis, maximums, label = "Maximum")
+plt.plot(x_axis, averages, label = "Average")
+plt.plot(x_axis, minimums, label = "Minimum")
 
 plt.xlabel('Number of Bidders')
 plt.ylabel('Price of Anarchy')
